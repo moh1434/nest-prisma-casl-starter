34027c1b7646344ea14fe376c415b72ec738cf31
moh1434 <moh1434.ma@gmail.com>
2023-03-22 12:14:51 +0300

    feat(prisma)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule],
   controllers: [],
   providers: [],
+  exports: [],
 })
 export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data: {
+        email: 'email@test.com',
+        password: 'not-hashed-yet',
+      },
+    });
+
+    return user;
+  }
+
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+    return users;
+  }
+
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: id.toString() },
+    });
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const newUser = await this.prisma.user.update({
+      where: { id: id },
+      data: updateUserDto,
+    });
+    return newUser;
+  }
+
+  async remove(id: string) {
+    const user = await this.prisma.user.delete({
+      where: {
+        id: id,
+      },
+      select: { id: true },
+    });
+    return user;
+  }
+}
