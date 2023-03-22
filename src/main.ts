e5b290913b145a18efff59824d7a6c1ca36d0b8c
moh1434 <moh1434.ma@gmail.com>
2023-03-22 12:15:05 +0300

    feat(Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable shutdown hook
+  const prismaService: PrismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  //s: Swagger
+  const config = new DocumentBuilder()
+    .setTitle('nest example')
+    .setDescription('My nest API description')
+    .setVersion('1.0')
+    .addTag('myTag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  //e: Swagger
   await app.listen(3000);
 }
 bootstrap();
