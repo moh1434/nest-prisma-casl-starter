9b50dbcd2eea0a32779aa97d581ef9e4caadf013
moh1434 <moh1434.ma@gmail.com>
2023-03-30 15:35:26 +0300

    use casl-prisma-generator

--- src/casl/generated/subjectsList.ts
+++ src/casl/generated/subjectsList.ts
@@ -1,7 +1,7 @@
+/** this file is auto generated, don't touch it **/
+
 import { Post } from '@prisma/client';
 import { TokenData } from 'src/auth/types-auth';
-
-/** this file is auto generated, don't touch it **/
 
 export type SubjectsList = {
   User: TokenData;
--- src/casl/generator.ts
+++ src/casl/generator.ts
@@ -1,90 +1,12 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import {
+  OverrideSubjects,
+  generateCaslSubjectsToFile,
+} from 'casl-prisma-generator/dist';
 
-function extractModelsNames(prismaSchemaPath) {
-  const schemaContent = fs.readFileSync(prismaSchemaPath).toString();
-
-  const regex = /model\s+(\w+)\s+{/g;
-  const matches: string[] = [];
-  let match;
-  while ((match = regex.exec(schemaContent)) !== null) {
-    matches.push(match[1]);
-  }
-
-  return matches; //['User', 'Post', ...]
-}
-
-export type OverrideModel = {
-  [key in string]: { typeName: string; importPath: string };
-};
-function generateCaslSubjectsList(
-  models: string[],
-  prismaClientPath: string,
-  overrides: OverrideModel = {},
-) {
-  for (const override of Object.keys(overrides)) {
-    if (!models.includes(override)) {
-      const error = `Override [${override}] is not in your prisma models`;
-      console.error(error);
-      throw new Error(error);
-    }
-  }
-
-  //
-  const customImports: string[] = [];
-  const prismaImports: string[] = [];
-
-  const content = [
-    "/** this file is auto generated, don't touch it **/\n",
-    'export type SubjectsList = {',
-  ];
-  models.map((model) => {
-    if (model in overrides) {
-      customImports.push(overrides[model].importPath);
-      content.push(`  ${model}: ${overrides[model].typeName};`);
-    } else {
-      prismaImports.push(model);
-      content.push(`  ${model}: ${model};`);
-    }
-  });
-  content.push('};');
-
-  const stringCustomImports = customImports.join('\n');
-  const stringPrismaImportsNames = prismaImports.join(', ');
-  const allPrismaImports = `import { ${stringPrismaImportsNames} } from '${prismaClientPath}';`;
-
-  const result =
-    allPrismaImports +
-    '\n' +
-    stringCustomImports +
-    '\n\n' +
-    content.join('\n') +
-    '\n';
-
-  return result;
-}
-
-const defaultPaths = {
-  outputPath: path.resolve(__dirname, 'generated/subjectsList.ts'),
-  prismaSchemaPath: 'prisma/schema.prisma',
-  prismaClientPath: '@prisma/client',
-};
-export function generateCaslSubjectsToFile(
-  overrides: OverrideModel = {},
-  paths: Partial<typeof defaultPaths> = defaultPaths,
-) {
-  paths = { ...defaultPaths, ...paths }; //merge paths
-
-  const prismaModels = extractModelsNames(paths.prismaSchemaPath);
-  fs.writeFileSync(
-    paths.outputPath,
-    generateCaslSubjectsList(prismaModels, paths.prismaClientPath, overrides),
-  );
-}
-
-generateCaslSubjectsToFile({
+const overrides: OverrideSubjects = {
   User: {
     typeName: 'TokenData',
     importPath: "import { TokenData } from 'src/auth/types-auth';",
   },
-});
+};
+generateCaslSubjectsToFile('src/casl/generated/subjectsList.ts', overrides);
