b326af1fc34a6b11c345fcd570dc85ce246eda24
moh1434 <moh1434.ma@gmail.com>
2023-05-14 17:03:35 +0300

    update TODO:

--- DEV/public/notes.ts
+++ DEV/public/notes.ts
@@ -1,8 +1,7 @@
 //TODO: Do it in order:
 
 //multiple global prisma DB (for apple test), nest support it per request?, i may decide it inside Jwt auth when it injects req.user
-// use argon2 instead of bicrypt
-//refresh token
+
 //redis
 //cacheWrapper
 //pm2 with logger
