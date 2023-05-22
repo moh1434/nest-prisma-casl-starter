14f0a500771a9182d03d93b6917d0f77ce2b623e
moh1434 <moh1434.ma@gmail.com>
2023-05-22 15:29:43 +0300

    dev(update notes)

--- DEV/public/notes.ts
+++ DEV/public/notes.ts
@@ -1,7 +1,5 @@
 //TODO: Do it in order:
 
 //multiple global prisma DB (for apple test), nest support it per request?, i may decide it inside Jwt auth when it injects req.user
-
-//redis
-//cacheWrapper
+//CASL errors not use our exceptions system
 //pm2 with logger
