ee2c67cf00a0e2470d4a6abf792ad74f62de4340
moh1434 <moh1434.ma@gmail.com>
2023-05-23 11:31:41 +0300

    fix(reject empty strings, reject 0 number)

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@nestjs/common';
 import {
   IsBoolean,
   IsEnum,
+  IsNotEmpty,
   IsNumber,
   IsString,
+  Min,
   validateSync,
 } from 'class-validator';
 
@@ -18,6 +20,7 @@ export class Env {
     this.validateEnv();
   }
   @IsNumber()
+  @Min(1)
   readonly port = Number(process.env.PORT || '3000');
   @IsEnum(NODE_ENV)
   readonly NODE_ENV = process.env.NODE_ENV;
@@ -26,31 +29,41 @@ export class Env {
   //
 
   @IsString()
+  @IsNotEmpty()
   readonly jwtSecret = process.env.JWT_SECRET;
 
   @IsNumber()
+  @Min(1)
   readonly jwtExpire = Number(process.env.JWT_EXPIRE_IN_HOUR) * 3600;
 
   @IsString()
+  @IsNotEmpty()
   readonly cookieSignKey = process.env.COOKIE_SIGN_KEY;
 
   @IsString()
+  @IsNotEmpty()
   readonly jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
 
   @IsNumber()
+  @Min(1)
   readonly jwtRefreshExpire =
     Number(process.env.JWT_REFRESH_EXPIRE_IN_DAY) * 86400;
 
   @IsString()
+  @IsNotEmpty()
   readonly frontendUrl = process.env.FRONTEND_URL;
 
   @IsString()
+  @IsNotEmpty()
   readonly S3_ACCESS_KEY = process.env.S3_ACCESS_KEY;
   @IsString()
+  @IsNotEmpty()
   readonly S3_SECRET_KEY = process.env.S3_SECRET_KEY;
   @IsString()
+  @IsNotEmpty()
   readonly S3_REGION = process.env.S3_REGION;
   @IsString()
+  @IsNotEmpty()
   readonly S3_BUCKET = process.env.S3_BUCKET;
 
   private validateEnv() {
