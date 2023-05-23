ce175b34036e030a8ffac2b03bd3968fee4c2d81
moh1434 <moh1434.ma@gmail.com>
2023-05-23 10:17:10 +0300

    refactor(notes file should not be .ts file)

--- DEV/public/notes.ts
+++
@@ -1,5 +0,0 @@
-//TODO: Do it in order:
-
-//multiple global prisma DB (for apple test), nest support it per request?, i may decide it inside Jwt auth when it injects req.user
-//pm2 with logger
-//add webpack config
