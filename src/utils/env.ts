77fb799b059b327335c6bb1d08f1a96510670cc1
moh1434 <moh1434.ma@gmail.com>
2023-05-26 19:57:51 +0300

    fix(Zod required types was marked as optional because "strictNullChecks"&"strict" was false in tsconfig)

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Injectable } from '@nestjs/common';
 import {
   IsBoolean,
@@ -21,50 +22,50 @@ export class Env {
   }
   @IsNumber()
   @Min(1)
-  readonly port = Number(process.env.PORT || '3000');
+  readonly port = Number(process.env.PORT! || '3000');
   @IsEnum(NODE_ENV)
-  readonly NODE_ENV = process.env.NODE_ENV;
+  readonly NODE_ENV = process.env.NODE_ENV!;
   @IsBoolean()
-  readonly isDebug = process.env.NODE_ENV != NODE_ENV.production;
+  readonly isDebug = process.env.NODE_ENV! != NODE_ENV.production!;
   //
 
   @IsString()
   @IsNotEmpty()
-  readonly jwtSecret = process.env.JWT_SECRET;
+  readonly jwtSecret = process.env.JWT_SECRET!;
 
   @IsNumber()
   @Min(1)
-  readonly jwtExpire = Number(process.env.JWT_EXPIRE_IN_HOUR) * 3600;
+  readonly jwtExpire = Number(process.env.JWT_EXPIRE_IN_HOUR!) * 3600;
 
   @IsString()
   @IsNotEmpty()
-  readonly cookieSignKey = process.env.COOKIE_SIGN_KEY;
+  readonly cookieSignKey = process.env.COOKIE_SIGN_KEY!;
 
   @IsString()
   @IsNotEmpty()
-  readonly jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
+  readonly jwtRefreshSecret = process.env.JWT_REFRESH_SECRET!;
 
   @IsNumber()
   @Min(1)
   readonly jwtRefreshExpire =
-    Number(process.env.JWT_REFRESH_EXPIRE_IN_DAY) * 86400;
+    Number(process.env.JWT_REFRESH_EXPIRE_IN_DAY!) * 86400;
 
   @IsString()
   @IsNotEmpty()
-  readonly frontendUrl = process.env.FRONTEND_URL;
+  readonly frontendUrl = process.env.FRONTEND_URL!;
 
   @IsString()
   @IsNotEmpty()
-  readonly S3_ACCESS_KEY = process.env.S3_ACCESS_KEY;
+  readonly S3_ACCESS_KEY = process.env.S3_ACCESS_KEY!;
   @IsString()
   @IsNotEmpty()
-  readonly S3_SECRET_KEY = process.env.S3_SECRET_KEY;
+  readonly S3_SECRET_KEY = process.env.S3_SECRET_KEY!;
   @IsString()
   @IsNotEmpty()
-  readonly S3_REGION = process.env.S3_REGION;
+  readonly S3_REGION = process.env.S3_REGION!;
   @IsString()
   @IsNotEmpty()
-  readonly S3_BUCKET = process.env.S3_BUCKET;
+  readonly S3_BUCKET = process.env.S3_BUCKET!;
 
   private validateEnv() {
     const errors = validateSync(this);
